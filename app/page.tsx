1f8319a6c8c15b11a2f6890a1b3167e807f90b93
RiyadhBenhallou <riadhriadh2004@gmail.com>
2024-09-19 15:11:24 +0200

    set up authentication with clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
+import { SignedOut, SignedIn, UserButton, SignInButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <>
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+        <button className="btn">Button</button>
+        <button className="btn btn-neutral">Neutral</button>
+        <button className="btn btn-primary">Primary</button>
+        <button className="btn btn-secondary">Secondary</button>
+        <button className="btn btn-accent">Accent</button>
+        <button className="btn btn-ghost">Ghost</button>
+        <button className="btn btn-link">Link</button>
+      </SignedIn>
     </>
   );
 }
