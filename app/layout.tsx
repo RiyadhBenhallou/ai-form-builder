cf6d7e33d3f894126e3db373c3427b354fa64c05
RiyadhBenhallou <riadhriadh2004@gmail.com>
2024-09-28 16:47:48 +0200

    added a toploader for loading state when browsing between different pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/toaster";
+import NextTopLoader from "nextjs-toploader";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,6 +32,7 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <NextTopLoader color="#2563eb" />
           <Toaster />
           {children}
         </body>
